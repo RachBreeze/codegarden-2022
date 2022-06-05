828510f8445574d59d478588312dade3b5a3df0a
Rachel Breeze <RachBreeze@users.noreply.github.com>
2022-06-05 10:55:15 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    myDirection = input.compassHeading()
+    if (myDirection < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (myDirection < 90) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (myDirection < 135) {
+        basic.showArrow(ArrowNames.West)
+    } else if (myDirection < 180) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (myDirection < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (myDirection < 270) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (myDirection < 315) {
+        basic.showArrow(ArrowNames.East)
+    } else {
+        basic.showArrow(ArrowNames.NorthEast)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("CodeGarden 2022, June 15 #H5YR")
+})
+input.onGesture(Gesture.Shake, function () {
+    images.iconImage(IconNames.SmallHeart).showImage(0)
+    images.iconImage(IconNames.Heart).showImage(0)
+    InvertedHeart.showImage(0)
+    images.iconImage(IconNames.Heart).showImage(0)
+})
+let myDirection = 0
+let InvertedHeart: Image = null
+InvertedHeart = images.createImage(`
+    # . # . #
+    . . . . .
+    . . . . .
+    # . . . #
+    # # . # #
+    `)
+images.iconImage(IconNames.Heart).showImage(0)
+basic.forever(function () {
+	
+})
